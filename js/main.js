55a5f1b6664bfc2e8b6e5fc6163786cce5d86e97
Aina <ainaeinikyte@gmail.com>
2024-02-14 14:17:12 +0200

    Update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+import { sumavimas, atimtis } from './components./sum.js';
+
+console.log('Labas rytas, Lietuva!');
+
+
+console.log(sumavimas(7, 5));
+console.log(sumavimas(-7, 5));
+console.log(sumavimas(7, -5));
+console.log(sumavimas(-7, -5));
+console.log(atimtis(7, 5));
+console.log(atimtis(-7, 5));
+console.log(atimtis(7, -5));
+console.log(atimtis(-7, -5));
