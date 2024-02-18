b006de8e61a4992514c57cff98b7d78f0dc0e65e
Aina <ainaeinikyte@gmail.com>
2024-02-18 20:42:51 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
-"use strict";
-import { sumavimas, atimtis } from './components/sum.js';
+const titleDOM = document.getElementById('title');
+console.log(titleDOM.innerText);
+console.log(titleDOM.textContent);
 
-console.log('Labas rytas, Lietuva!');
+titleDOM.innerText = 'Kaip gaminami blynai?';
+titleDOM.textContent = 'Kaip gaminami blynai???';
+
+const ingridientsDOM = document
+    .getElementsByTagName('ul')[0]
+    .getElementsByTagName('li');
+console.log(ingridientsDOM);
+
+console.log(ingridientsDOM[0].innerText);
+console.log(ingridientsDOM[1].innerText);
+console.log(ingridientsDOM[2].innerText);
+console.log(ingridientsDOM[3].innerText);
+
+const ingridients = [];
+
+// for (let i = 0; i < ingridientsDOM.length; i++) {
+//    const ingridientDOM = ingridientsDOM[i];
+//   ingridients.push(ingridientDOM.innerText);
+// }
 
 
-console.log(sumavimas(7, 5));
-console.log(sumavimas(-7, 5));
-console.log(sumavimas(7, -5));
-console.log(sumavimas(-7, -5));
-console.log(atimtis(7, 5));
-console.log(atimtis(-7, 5));
-console.log(atimtis(7, -5));
-console.log(atimtis(-7, -5));
+// for (const ingridientDOM of ingridientsDOM) {
+//    ingridients.push(ingridientDOM.innerText);
+// }
+
+const ingridients = [...ingridientsDOM].map(
+    ingridientDOM => ingridientDOM.innerText
+);
+
+console.log(ingridients);
