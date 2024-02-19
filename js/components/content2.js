a4458e31f827b8076f19ec03413cc4507d2ffb2a
Aina <ainaeinikyte@gmail.com>
2024-02-19 13:36:43 +0200

    Update

---
+++ js/components/content2.js
@@ -0,0 +1,71 @@
+const data = [
+  {
+    title: "Ingredientai",
+    list: ["Miltai", "", "Druska", "Vanduo", "Kiaušiniai"],
+  },
+  {
+    title: "Įranga/įrankiai",
+    list: ["Mikseris", "        ", "Keptuvė", " ", "Šaukštas", "Lėkštė"],
+  },
+];
+
+const contentDOM = document.getElementById("content");
+
+let HTML = "";
+
+for (const column of data) {
+  let listHTML = "";
+
+  for (const item of column.list) {
+    if (item.trim().length === 0) {
+      continue;
+    }
+
+    listHTML += `<li>${item}</li>`;
+  }
+
+  HTML += `<div class="column">
+                <h2>${column.title}</h2>
+                <ul class="list">
+                    ${listHTML}
+                </ul>
+            </div>`;
+}
+
+contentDOM.innerHTML = HTML;
+
+
+const data1 = [
+    [
+        {
+            title: "Ingredientai",
+            list: ["Miltai", "", "Druska", "Vanduo", "Kiaušiniai"],
+        },
+        {
+            title: "Įranga/įrankiai",
+            list: ["Mikseris", "        ", "Keptuvė", " ", "Šaukštas", "Lėkštė"],
+        },
+    ],
+    [
+        {
+            title: "Ingredientai",
+            list: ["Miltai", "", "Druska", "Vanduo", "Kiaušiniai"],
+        },
+        {
+            title: "Įranga/įrankiai",
+            list: ["Mikseris", "        ", "Keptuvė", " ", "Šaukštas", "Lėkštė"],
+        },
+    ]
+]
+
+
+function generateRowHTML(rowData) {
+  let rowHTML = "";
+
+  for (const columnData of rowData) {
+    const { title, list } = columnData;
+    rowHTML += generateColumnHTML(title, list);
+  }
+
+  return `<div class="row">${rowHTML}</div>`;
+}
